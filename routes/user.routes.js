b5a30f9f41546f9d0ba8adc643b265972b704c09
Nehal Kadyan <nehalkadyan@Nehals-MacBook-Air.local>
2025-10-28 10:50:18 +0530

    signin api

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,7 +3,8 @@
 const express = require("express");
 // require bcrypt
 
-const bcrypt = require("bcryptjs")
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 // import User model
 
@@ -37,6 +38,48 @@ router.post("/store-user", async function (req, res) {
     console.log(err);
   }
 });
+
+// api to singin
+
+
+router.post("/signin", async (req, res) => {
+  try{
+     const {email, password} = req.body;
+      //.  abc@gmail.com
+      // password = 123
+
+     // check whther user exists in the db
+
+     const existingUser = await User.findOne({email});
+
+     if(!existingUser){
+        return res.status(404).json({message : "User not found with this email"})
+     }
+
+     // compare password
+
+     const isValidPassword = await bcrypt.compare(password, existingUser.password)
+
+     if(!isValidPassword){
+      return res.status(401).json({message : "Password did not match"})
+    }
+
+    // create token
+
+    const tokenData = {
+      id: existingUser._id
+    }
+
+    const token = jwt.sign(tokenData, process.env.JWT_SECRET_KEY, {expiresIn: "1h"})
+
+    return res.json({message : "user signed in successfully", user : existingUser, token})
+
+
+  }catch(err){
+    console.log("error", err)
+  }
+})
+
 
 // api to fetch users
 
